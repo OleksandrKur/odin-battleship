4963d3c1903802f1f2eae0df654e90068dd4951c
OleksandKur <136488587+OleksandrKur@users.noreply.github.com>
2024-12-05 15:48:35 +0200

    Setup: initialize npm and add webpack with babel and jest. Add file structure

---
+++ webpack.dev.js
@@ -0,0 +1,20 @@
+const { merge } = require("webpack-merge");
+const common = require("./webpack.common.js");
+
+module.exports = merge(common, {
+  mode: "development",
+  devtool: "eval-source-map",
+  devServer: {
+    watchFiles: ["./src/template.html"],
+  },
+  watchOptions: {
+    aggregateTimeout: 200,
+    poll: 1000,
+  },
+});
+const { merge } = require("webpack-merge");
+const common = require("./webpack.common.js");
+
+module.exports = merge(common, {
+  mode: "production",
+});
